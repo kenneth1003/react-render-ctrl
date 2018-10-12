7f26f25127236bf38749102bd539e156a1e77eac
Kenneth Cheung <kenneth@Kenneths-MacBook-Pro.local>
2018-10-13 02:31:23 +0800

    [fix] version number

--- dist/constant/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var localDefaultLoadingId = exports.localDefaultLoadingId = 'ldl';
-var localDefaultErrorId = exports.localDefaultErrorId = 'lder';
-var localDefaultEmptyId = exports.localDefaultEmptyId = 'ldem';
